0687170aff3223eba0e5aeb93a4be51cecde4125
Keith Kwan <keith@Keiths-MacBook-Air.local>
2026-02-24 21:29:36 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import useSWR from 'swr'
 import DogBreeds from './components/DogBreeds'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function App() {
